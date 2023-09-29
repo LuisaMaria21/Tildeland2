c7863a5152484ccd8eeec415be54106eed5443f8
LuisaMaria21 <88382889+LuisaMaria21@users.noreply.github.com>
2023-09-28 23:32:21 -0500

    scripts

--- Assets/Scripts/CambioCanva.cs
+++ Assets/Scripts/CambioCanva.cs
@@ -8,25 +8,9 @@ public class CambioCanva : MonoBehaviour
     public GameObject canvas1; // Arrastra el primer Canvas aqui en el Inspector
     public GameObject canvas2; // Arrastra el segundo Canvas aqui en el Inspector
     public Button botonCambio;
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     // Al iniciar el juego, mostramos el primer Canvas y ocultamos el segundo Canvas
-    //     canvas1.SetActive(true);
-    //     canvas2.SetActive(false);
 
-    //     // Configura la funcion de cambio cuando se hace clic en el boton
-    //     botonCambio.onClick.AddListener(CambiarCanvas);
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     canvas1.SetActive(!canvas1.activeSelf); // Cambiamos el estado del primer Canvas
-    //     canvas2.SetActive(!canvas2.activeSelf); // Cambiamos el estado del segundo Canvas
-    // }
-
-    public void CambiarCanvas(){
+    public void CambiarCanvas()
+    {
         canvas1.SetActive(true);
         canvas2.SetActive(false);
     }
--- Assets/Scripts/LogicaVolumen.cs
+++ Assets/Scripts/LogicaVolumen.cs
@@ -1,13 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogicaVolumen : MonoBehaviour
 {
+    public slider;
+    public float sliderValue;
+    public Image imagenMute;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume= slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        if (sliderValue == 0)
+        {
+            imagenMute.enable = true;
+        }
+        else
+        {
+            imagenMute.enable = false;
+        }
     }
 
     // Update is called once per frame
